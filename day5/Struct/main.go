9335e91eb16cf243c607304b79e46f687c5d3e58
Yiwen Liu <liuyiwen@primecloud.cn>
2019-03-28 18:07:52 +0800

    add Struct

---
+++ day5/Struct/main.go
@@ -0,0 +1,29 @@
+package main
+
+//定义构造体
+type user struct {
+	id int
+	username string
+	password string
+}
+
+func main()  {
+	//初始化构造体
+	var user1 user
+	user1.id =22
+	user1.username = "lalala"
+	user1.password = "aaa"
+	//初始化构造体方法2
+	var user2 *user= &user{
+		id:23,
+		username:"balala",
+		password:"wulala",
+	}
+
+	//初始化构造体方法3
+	user3 := user{
+		id:666,
+		username:"wahaha",
+		password:"666666",
+	}
+}
