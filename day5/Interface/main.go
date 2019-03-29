09aa31d0e3fccb12de7c0821a98e53e3696082f5
Yiwen Liu <liuyiwen@primecloud.cn>
2019-03-29 15:28:13 +0800

    add Interface

---
+++ day5/Interface/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+type student struct {
+	id int
+	name string
+}
+
+//interface 中只能有方法,不能有变量
+type user interface {
+	print()
+}
+
+func (aa *student) print()  {
+	fmt.Println(aa.id)
+	fmt.Println(aa.name)
+}
+
+
+func main() {
+	var stu student
+	var user1 user
+	stu.id = 1
+	stu.name = "lalala"
+	//因为stu是指针类型实现的,所以如果接口想调用student构造体,必须也要使用指针
+	user1 = &stu
+	user1.print()
+}
